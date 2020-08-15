98b98e733579a944918415a16f3e41b32adc5960
Pablo Tabares H <pablother3@gmail.com>
2020-08-15 16:39:12 -0500

    initialprops added

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import axios from 'axios'
 
-const User = () => {
+const User = ({todos}) => {
   return (
     <Layout>
-      USer page
+      {JSON.stringify(todos)}
     </Layout>
   )
 }
+
+User.getInitialProps = async (context) => {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`)
+
+  return {
+    todos: response.data
+  }
+}
+
 
 export default User

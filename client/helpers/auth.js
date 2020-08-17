fc07a2ac54c4e5cd41bc4d0741ffad89a78c62f4
Pablo Tabares H <pablother3@gmail.com>
2020-08-16 19:18:59 -0500

    token get from server

--- client/helpers/auth.js
+++ client/helpers/auth.js
@@ -1,67 +1,88 @@
-import cookie from 'js-cookie';
-import Router from 'next/router';
+import cookie from "js-cookie";
+import Router from "next/router";
 
 // Set in cookie
 export const setCookie = (key, value) => {
   if (process.browser) {
     cookie.set(key, value, {
-      expires: 1
-    })
+      expires: 1,
+    });
   }
-}
+};
 
 // Remove from cookie
 export const removeCookie = (key) => {
   if (process.browser) {
-    cookie.remove(key)
+    cookie.remove(key);
   }
-}
+};
 
 // Get from cookie such as stored token
 // Will be useful when we need to make request to server with auth token
-export const getCookie = (key) => {
-  if (process.browser) {
+export const getCookie = (key, req) => {
+  /* if (process.browser) {
     return cookie.get(key)
+  } */
+  return process.browser
+    ? getCookieFromBrowser(key)
+    : getCookieFromServer(key, req);
+};
+
+export const getCookieFromBrowser = (key) => {
+  return cookie.get(key);
+};
+
+export const getCookieFromServer = (key, req) => {
+  if (!req.headers.cookie) {
+    return undefined;
   }
-}
+  const token = req.headers.cookie
+    .split(";")
+    .find(c => c.trim().startsWith(`${key}=`));
+  if (!token) return undefined;
+
+  let tokenValue = token.split("=")[1];
+  console.log("cookie form server", tokenValue);
+  return tokenValue;
+};
 
 // Set in localstorage
 export const setLocalStorage = (key, value) => {
   if (process.browser) {
-    localStorage.setItem(key, JSON.stringify(value))
+    localStorage.setItem(key, JSON.stringify(value));
   }
-}
+};
 
 // rRemove from localstorage
 export const removeLocalStorage = (key) => {
   if (process.browser) {
-    localStorage.removeItem(key)
+    localStorage.removeItem(key);
   }
-}
+};
 
 // Atuherticate user by passing data to cookie and localstorage during signin
 export const authenticate = (response, next) => {
-  setCookie('token', response.data.token);
-  setLocalStorage('user', response.data.user);
+  setCookie("token", response.data.token);
+  setLocalStorage("user", response.data.user);
   next();
-}
+};
 
 // access user info from localstorage
 export const isAuth = () => {
   if (process.browser) {
-    const cookieChecked = getCookie('token')
-    const user = localStorage.getItem('user');
+    const cookieChecked = getCookie("token");
+    const user = localStorage.getItem("user");
     if (cookieChecked && user) {
-      return JSON.parse(user)
+      return JSON.parse(user);
     } else {
-      return false
+      return false;
     }
   }
-}
+};
 
 export const logout = () => {
-  removeLocalStorage('user');
-  removeCookie('token');
+  removeLocalStorage("user");
+  removeCookie("token");
 
-  Router.push('/')
-}
+  Router.push("/");
+};
--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import Layout from '../../components/Layout'
 import axios from 'axios'
+import Layout from '../../components/Layout'
+import { getCookie } from '../../helpers/auth'
 
-const User = ({todos}) => {
+const User = ({user}) => {
   return (
     <Layout>
-      {JSON.stringify(todos)}
+      {JSON.stringify(user)}
     </Layout>
   )
 }
 
 User.getInitialProps = async (context) => {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`)
+  console.log(context)
+  const token = getCookie('token', context.req)
 
-  return {
-    todos: response.data
+  try {
+    const response = await axios.get(`${process.env.API}/user`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        contentType: 'application/json' 
+      }
+    })
+
+    return {
+      user: response.data
+    }
+  } catch (error) {
+    if (error.response.status === 401) {
+      return {
+        user: 'no-user'
+      }
+    }
   }
 }
 
